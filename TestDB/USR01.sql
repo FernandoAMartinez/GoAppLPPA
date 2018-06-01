626c06d9e8442840b863c05ce713193108f284da
Fernando Martinez <39782229+FernandoAMartinez@users.noreply.github.com>
2018-06-01 16:35:59 -0300

    Test Database: Users Table
    USR01 Create Table Script

---
+++ TestDB/USR01.sql
@@ -0,0 +1,20 @@
+﻿/****************************************************************************************/
+/****************************		 TABLA USUARIOS		   ******************************/
+/****************************************************************************************/
+CREATE TABLE USR01(
+-- DATOS DE SESIÓN
+USRNAME		CHAR(11)	PRIMARY KEY		NOT NULL, --NOMBRE DE USUARIO
+USPASS		CHAR(50)					NOT NULL, --CONTRASEÑA
+-- DATOS DEL USUARIO
+FNAME		CHAR(50)					NOT NULL, --NOMBRES
+LNAME		CHAR(50)					NOT NULL, --APELLIDOS
+BDATE		DATE						NOT NULL, --FECHA DE NACIMIENTO
+SEX			CHAR(1)						NOT NULL, --SEXO
+-- DATOS DE CONTROL DE SESIÓN
+USTRIES		INT							NOT NULL, --INTENTOS DE INICIO DE SESION
+ISBLOCKED	CHAR(1)						NOT NULL, --MARCA DE BLOQUEO DE USUARIO
+ISRECOV		CHAR(1)						NOT NULL, --MARCA DE CONTRASEÑA RECUPERADA
+-- DÍGITO VERIFICADOR DE REGISTRO
+DVH			INT							NOT NULL  --DIGITO VERIFICADOR HORIZONTAL
+)
+
