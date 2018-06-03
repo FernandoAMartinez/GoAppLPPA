25e2173315e077d176cf6a6ae16cd351c4f7b9fb
FernandoAMartinez <fernandoandres.martinez@alumnos.uai.edu.ar>
2018-06-02 21:06:10 -0300

    Model Classes v1.1

---
+++ Modelo/dataAccess.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Modelo
+{
+    public class dataAccess
+    {
+        #region Objects
+        SqlConnection CN = new SqlConnection(ConfigurationManager.ConnectionStrings["TEST"].ConnectionString);
+        SqlCommand CMD = new SqlCommand();
+        SqlDataAdapter DA = new SqlDataAdapter();
+        #endregion
+
+        #region Singleton
+        private static dataAccess _Instance = null;
+
+        public static dataAccess GetInstance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new Modelo.dataAccess();
+                }
+                return _Instance;
+            }
+        }
+        private dataAccess()
+        {
+            //nothing
+        }
+
+
+
+        #endregion
+
+        #region Methods
+        public void Open()
+        {
+            try
+            {
+                CN.Open();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void Close()
+        {
+            try
+            {
+                CN.Close();
+                GC.Collect();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public int ExecSP(SqlCommand CMD)
+        {
+            int n;
+            Open();
+            CMD.Connection = CN;
+            CMD.CommandType = CommandType.StoredProcedure;
+            n = CMD.ExecuteNonQuery();
+            Close();
+            return n;
+        }
+
+        public int ExeNonQuery(string query)
+        {
+            int n;
+            Open();
+            CMD.Connection = CN;
+            CMD.CommandType = CommandType.Text;
+            CMD.CommandText = query;
+            n = CMD.ExecuteNonQuery();
+            Close();
+            return n;
+        }
+
+        public int ExeScalarInt(string query)
+        {
+            int n;
+            object obj;
+            Open();
+            CMD.Connection = CN;
+            CMD.CommandType = CommandType.Text;
+            CMD.CommandText = query;
+            obj = CMD.ExecuteScalar();
+            Close();
+            n = Convert.ToInt32(obj);
+            return n;
+        }
+
+        public string ExeScalarString(string query)
+        {
+            string n;
+            object obj;
+            Open();
+            CMD.Connection = CN;
+            CMD.CommandType = CommandType.Text;
+            CMD.CommandText = query;
+            obj = CMD.ExecuteScalar();
+            Close();
+            n = obj.ToString();
+            return n;
+        }
+
+        public DataSet ExecDS(string query)
+        {
+            Open();
+            SqlDataAdapter DA = new SqlDataAdapter(query, CN);
+            DataSet DS = new DataSet();
+            DA.Fill(DS);
+            DS.Dispose();
+            Close();
+            return DS;
+        }
+
+        SqlDataReader DR;
+
+        public string ExecDR(string query)
+        {
+            string res = "";
+            Open();
+            CMD.Connection = CN;
+            CMD.CommandType = CommandType.Text;
+            CMD.CommandText = query;
+            DR = CMD.ExecuteReader();
+            while (DR.Read() == true)
+            {
+                //res = DR.ToString();
+                res = DR.GetString(0);
+            }
+            Close();
+            return res;
+        }
+        #endregion
+    }
+}
---
+++ Modelo/dataManipulation.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Modelo
+{
+    public class dataManipulation
+    {
+        #region Objects
+
+        #endregion
+
+        #region Singleton
+        private static dataManipulation _Instance = null;
+
+        public static dataManipulation GetInstance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new dataManipulation();
+                }
+                return _Instance;
+            }
+        }
+        private dataManipulation()
+        {
+            //nothing
+        }
+        #endregion
+
+
+        public int usp_LoginValidation(string Usu, string Pass)
+        {
+            //dataAcccess dA = new dataAcccess();
+            SqlCommand CMD = new SqlCommand();
+            dataAccess.GetInstance.Open();
+            CMD.CommandType = CommandType.StoredProcedure;
+            CMD.Parameters.Add(new SqlParameter("@Usu", Usu));
+            CMD.Parameters.Add(new SqlParameter("@Pass", Pass));
+            CMD.CommandText = "usp_LoginValidation";
+            dataAccess.GetInstance.Close();
+            return dataAccess.GetInstance.ExecSP(CMD);
+        }
+
+        public int usp_BlockUser(string Usu)
+        {
+            //dataAcccess dA = new Model.dataAcccess();
+            SqlCommand CMD = new SqlCommand();
+            dataAccess.GetInstance.Open();
+            CMD.CommandType = CommandType.StoredProcedure;
+            CMD.Parameters.Add(new SqlParameter("@usu", Usu));
+            CMD.CommandText = "usp_BlockUser";
+            dataAccess.GetInstance.Close();
+            return dataAccess.GetInstance.ExecSP(CMD);
+        }
+
+        public int usp_ResetTries(string Usu)
+        {
+            //dataAcccess dA = new Model.dataAcccess();
+            SqlCommand CMD = new SqlCommand();
+            dataAccess.GetInstance.Open();
+            CMD.CommandType = CommandType.StoredProcedure;
+            CMD.Parameters.Add(new SqlParameter("@usu", Usu));
+            CMD.CommandText = "usp_ResetTries";
+            dataAccess.GetInstance.Close();
+            return dataAccess.GetInstance.ExecSP(CMD);
+        }
+
+        public int usp_setBitacora(int Bit, string Action, int Priority, string Type, string Class, string Usu, DateTime Date, DateTime Time)
+        {
+            //dataAcccess dA = new dataAcccess();
+            SqlCommand CMD = new SqlCommand();
+            dataAccess.GetInstance.Open();
+            CMD.CommandType = CommandType.StoredProcedure;
+            CMD.Parameters.Add(new SqlParameter("@BIT", Bit));
+            CMD.Parameters.Add(new SqlParameter("@ACTION", Action));
+            CMD.Parameters.Add(new SqlParameter("@PRIORITY", Priority));
+            CMD.Parameters.Add(new SqlParameter("@TYPE", Type));
+            CMD.Parameters.Add(new SqlParameter("@CLASS", Class));
+            CMD.Parameters.Add(new SqlParameter("@USER", Usu));
+            CMD.Parameters.Add(new SqlParameter("@DATE", Date));
+            CMD.Parameters.Add(new SqlParameter("@TIME", Time));
+            CMD.CommandText = "usp_setBitacora";
+            dataAccess.GetInstance.Close();
+            return dataAccess.GetInstance.ExecSP(CMD);
+        }
+
+
+    }
+}
