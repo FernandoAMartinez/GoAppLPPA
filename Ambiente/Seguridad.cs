19b4b2a38cb1c7b6a28685e1d0d53e87769e0459
FernandoAMartinez <fernandoandres.martinez@alumnos.uai.edu.ar>
2018-06-02 21:05:40 -0300

    MD5 Encription Methods to "Seguridad" class

---
+++ Ambiente/Seguridad.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Ambiente
+{
+    public class Seguridad
+    {
+
+        #region Singleton
+        private static Seguridad _Instance = null;
+
+        public static Seguridad GetInstance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new Seguridad();
+                }
+                return _Instance;
+            }
+        }
+        private Seguridad()
+        {
+            //nothing
+        }
+
+
+
+        #endregion
+
+        MD5 c_MD5 = MD5CryptoServiceProvider.Create();
+        ASCIIEncoding encoding = new ASCIIEncoding();
+
+        private byte[] b_Stream;
+
+        public byte[] Stream
+        {
+            get { return b_Stream; }
+            set { b_Stream = value; }
+        }
+
+        public string GetMD5(string str)
+        {
+            Stream = null;
+            StringBuilder sb = new StringBuilder();
+            Stream = c_MD5.ComputeHash(encoding.GetBytes(str));
+            for (int i = 0; i < Stream.Length; i++) sb.AppendFormat("{0:x2}", Stream[i]);
+            return sb.ToString();
+
+        }
+    }
+}
